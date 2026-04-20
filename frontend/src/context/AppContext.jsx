220c25068a3dff0504cff9b8ab1823d6a1dc8562
Gayatri414 <gayatrigade7949@gmail.com>
2026-04-20 23:36:38 +0530

    add changes of AppContext.jsx file

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,12 +1,42 @@
-import {createContext} from "react";
+import {createContext, useState,useEffect} from "react";
 import {doctors} from "../assets/assets";
-
+import axios from "axios";
+import {toast} from 'react-toastify'
 export const AppContext=createContext();
 const AppContextProvider=(props)=>{
     const currencySymbol='$'
-    const value={
-        doctors,currencySymbol
+      const backendUrl=import.meta.env.VITE_BACKEND_URL;
+      const[doctors,setDoctors]=useState([])
+
+const [token,setToken]=useState('')
+
+
+
+
+
+
+
+   
+    console.log("Backend URL:", backendUrl);
+  const getDoctorsData=async()=>{
+    try{
+const {data}=await axios.get(backendUrl+'/api/doctor/list')
+if(data.success){
+setDoctors(data.doctors)
+}else{
+    toast.error(data.message)
+}
+    }catch(error){
+console.log(error);
+toast.error(error?.response?.data?.message || error.message)
     }
+  }
+  const value={
+        doctors,currencySymbol,token,setToken,backendUrl
+    }
+  useEffect(()=>{
+    getDoctorsData()
+  },[])
     return(
         <AppContext.Provider value={value}>
 {props.children}
